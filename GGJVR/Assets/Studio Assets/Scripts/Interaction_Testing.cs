ff84fe7745c5be5ea56a926cc7784973e64d74a0
Dan <dmaccormick@hotmail.com>
2020-02-01 17:01:14 -0500

    Set up the button pressing interaction with a test script
    - Hooked test script into button up, down, and pressed events

---
+++ GGJVR/Assets/Studio Assets/Scripts/Interaction_Testing.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Interaction_Testing : MonoBehaviour
+{
+    //--- Button Interactions ---//
+    public void OnButtonDown()
+    {
+        Debug.Log("OnButtonDown()");
+    }
+
+    public void OnButtonUp()
+    {
+        Debug.Log("OnButtonUp()");
+    }
+
+    public void OnButtonIsPressed()
+    {
+        Debug.Log("OnButtonIsPressed()");
+    }
+}
